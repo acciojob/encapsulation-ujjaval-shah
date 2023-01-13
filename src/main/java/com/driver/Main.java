c1f2dcd4138dd0d09e281ccc9b2c2469c134ef5d
ujjaval-shah <uss1999int@gmail.com>
2023-01-13 19:57:38 +0530

    test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,12 +2,12 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        RWOnly rw = new RWOnly();
+        // RWOnly rw = new RWOnly();
 
-        // The field RWOnly.v is not visible
-        // System.out.println(rw.v);
+        // // The field RWOnly.v is not visible
+        // // System.out.println(rw.v);
 
-        rw.setName("a new name");
-        System.out.println(rw.getName());
+        // rw.setName("a new name");
+        // System.out.println(rw.getName());
     }
 }
