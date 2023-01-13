f490b7b9ce1d0fa5baa1f77a8ad45adac80508b1
ujjaval-shah <uss1999int@gmail.com>
2023-01-13 19:49:47 +0530

    Problem 3 solved.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        // The field RWOnly.v is not visible
+        // System.out.println(rw.v);
+
+        rw.setV(100);
+        System.out.println(rw.getV());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int v;
+
+    public void setV(int num) {
+        v = num;
+    }
+
+    public int getV() {
+        return v;
+    }
 }
